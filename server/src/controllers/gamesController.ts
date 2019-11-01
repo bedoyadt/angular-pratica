77eb54665f32e6216c8606f9cfde7a8d27653ec4
yeison bedoya <bedoyadt@gmail.com>
2019-11-01 18:01:29 -0500

    REST API terminado

---
+++ server/src/controllers/gamesController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+
+
+import pool from '../database';
+
+class GamesController {
+
+    public async list(req: Request, res: Response): Promise<void> {
+        const games = await pool.query('SELECT * FROM games');
+        res.json(games);
+    }
+
+    public async getOne(req: Request, res: Response) {
+        const { id } = req.params;
+        const games = await pool.query('SELECT * FROM games WHERE id = ?', [id]);
+        console.log(games.length);
+        if (games.length > 0) {
+            return res.json(games[0]);
+        }
+        res.status(404).json({ text: "El Fuego no Esisted" });
+    }
+
+    public async create(req: Request, res: Response) {
+        const result = await pool.query('INSERT INTO games set ?', [req.body]);
+        res.json({ message: 'Furgo Guardado' });
+    }
+
+    public async update(req: Request, res: Response) {
+        const { id } = req.params;
+        const oldGame = req.body;
+        await pool.query('UPDATE games set ? WHERE id = ?', [req.body, id]);
+        res.json({ message: "El juego fue actualizado" });
+    }
+
+    public async delete(req: Request, res: Response){
+        const { id } = req.params;
+        await pool.query('DELETE FROM games WHERE id = ?', [id]);
+        res.json({ message: "El juego fue eliminado" });
+    }
+}
+
+const gamesController = new GamesController;
+export default gamesController;
