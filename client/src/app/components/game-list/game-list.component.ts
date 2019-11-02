78b095124a3d3ce04098b180c8c165cac69d97e0
yeison bedoya <bedoyadt@gmail.com>
2019-11-02 13:38:17 -0500

    terminando la Vista con angular

---
+++ client/src/app/components/game-list/game-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import { GamesService } from '../../services/games.service';
+import gamesController from '../../../../../server/src/controllers/gamesController';
+
+@Component({
+  selector: 'app-game-list',
+  templateUrl: './game-list.component.html',
+  styleUrls: ['./game-list.component.css']
+})
+export class GameListComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
+
+  games: any = [];
+
+  constructor(private gamesService: GamesService) { }
+
+  ngOnInit() {
+    this.getGames();
+  }
+
+getGames() {
+  this.gamesService.getGames().subscribe(
+    res => {
+      this.games = res;
+    },
+    err => console.error(err)
+  );
+}
+
+
+  deleteGame(id: string){
+this.gamesService.deleteGame(id).subscribe(
+  res => {
+    this.getGames();
+  },
+  err => console.error(err)
+);
+
+
+
+
+
+  }
+}
